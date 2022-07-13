c868ba0572b45a01f225b32f4e76c4aaac5dcdd9
Zehntech26 <106737786+Zehntech26@users.noreply.github.com>
2022-07-13 14:46:30 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,7 @@
+$(".download-btn").click(function () {
+  $(".download").toggleClass("download--animate");
+  $(".circle").toggleClass("circle--animate");
+  $(".bar").toggleClass("bar--animate");
+  $(".btn__stop").toggleClass("btn__stop--animate");
+  $(".btn__done").toggleClass("btn__done--animate");
+});
